798cc131a72b099844558c478bc3343bc1ecce86
sreejith-naren <sreejith@hamon.in>
2023-03-01 21:01:56 +0530

    initial commit

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func SetUpRouter() *gin.Engine{
+    router := gin.Default()
+    return router
+}
+
+func TestGetAlbums(t *testing.T) {
+    r := SetUpRouter()
+    r.GET("/albums", GetAlbums)
+    req, _ := http.NewRequest("GET", "/albums", nil)
+    w := httptest.NewRecorder()
+    r.ServeHTTP(w, req)
+
+    var albums []Album
+    json.Unmarshal(w.Body.Bytes(), &albums)
+
+    assert.Equal(t, http.StatusOK, w.Code)
+    assert.NotEmpty(t, albums)
+}
